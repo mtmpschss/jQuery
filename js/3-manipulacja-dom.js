f5ec87637bbfb6dab089732cd5a0d5706e2f02dd
Kamil 1st <kamilferst@wp.pl>
2018-01-14 12:39:00 +0100

    manipulacja html

---
+++ js/3-manipulacja-dom.js
@@ -0,0 +1,37 @@
+'use strict';
+
+
+
+$(function() {
+    
+/*//    var textParagrafu = $("p#paragraf").text("Nowy tekst paragrafu");
+    
+    var jakisTamParagraf = $("p.paragrafnext").html("<b>Hello</b> world!!!").after("<p>Treść za elementem</p>");
+    
+    jakisTamParagraf.append("</br> dodatkowy text");
+    
+    $("h3").remove();
+    
+    $("#second").empty();
+    
+    $("#second").html("<span style='color: red;'>Kolorowy tekst w spanie</span>");
+    
+    $("h1").css({"font-size":"70px", "color":"lime"});
+    
+    console.log(jakisTamParagraf);
+    
+    
+    var imie = $("input#immie").val();
+    console.log(imie);
+    
+  $("input#immie").val("Adam");
+    
+    
+    $("h1").attr("title"); //pobiera atrybut
+ $("h1").attr("title", "Nagłówek h1"); //ustawia nowy naglowek*/
+    
+    
+    $("h1").addClass("blue");
+    $(".paragrafnext").removeClass("paragrafnext");
+    
+});
