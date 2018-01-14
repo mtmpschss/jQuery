fbc5da2bffc6871105ba3be733f0dee68fbb5bad
Kamil 1st <kamilferst@wp.pl>
2018-01-14 10:54:50 +0100

    wstep do jquery

---
+++ js/1-document-ready.js
@@ -0,0 +1,19 @@
+'use strict';
+
+
+/*$(document).ready(function() {
+
+              alert("czołem, wczytałem dom i jestem jquery");    
+                  
+});*/
+
+
+$(function() {
+    
+//    $("#paragraf").css("color", "red");
+    
+    $("#paragraf").css(
+        {"color": "red",
+         "background-color":"#000"});
+    
+});
