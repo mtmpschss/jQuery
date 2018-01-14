109b9c897f0b5ca047be4d852624c9a5c3eb86ee
Kamil 1st <kamilferst@wp.pl>
2018-01-14 11:17:21 +0100

    jquery selektory

---
+++ js/2-selektory.js
@@ -0,0 +1,12 @@
+    'use strict';
+
+$(function() {
+   
+    var paragrafy = $("p");
+    var naglowekH2 = $("#second");
+    var paragrafKlasa = $(".paragraf");
+    
+    
+    console.log(paragrafKlasa);
+    
+});
